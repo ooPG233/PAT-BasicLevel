7f888703b53d6d69e566f4230debad55ea95c9b5
ooPG <oopg@foxmail.com>
2021-03-28 20:36:29 +0800

    1002 用字典类型优化了算法

--- ooPG/1002.py
+++ ooPG/1002.py
@@ -1,11 +1,11 @@
 from functools import reduce
 def main():
     input_n=input()
-    spell_list=['ling','yi','er','san','si','wu','liu','qi','ba','jiu']
+    spell_dict={'0':"ling",'1':"yi",'2':"er",'3':"san",'4':"si",'5':"wu",'6':"liu",'7':"qi",'8':"ba",'9':"jiu"}
     
     sum_n = lambda n : str(reduce(lambda x,y:x+y,[int(i) for i in input_n]))
 
-    spell = lambda n : ' '.join([spell_list[int(i)] for i in sum_n(n)])
+    spell = lambda n : ' '.join([spell_dict[i] for i in sum_n(n)])
     
     print(spell(input_n))
 
