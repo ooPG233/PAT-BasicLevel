d450b16ab0c400a30801e85c86adba5004e4a184
ooPG <oopg@foxmail.com>
2021-04-05 05:40:31 +0800

    注释

--- ooPG/1002.py
+++ ooPG/1002.py
@@ -3,7 +3,7 @@ def main():
     input_n=input()
     spell_dict={'0':"ling",'1':"yi",'2':"er",'3':"san",'4':"si",'5':"wu",'6':"liu",'7':"qi",'8':"ba",'9':"jiu"}
     
-    sum_n = lambda n : str(reduce(lambda x,y:x+y,[int(i) for i in input_n]))
+    sum_n = lambda n : str(reduce(lambda x,y:x+y,[int(i) for i in input_n])) #加和
 
     spell = lambda n : ' '.join([spell_dict[i] for i in sum_n(n)])
     
