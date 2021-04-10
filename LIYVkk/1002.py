8e499a7dbd7925f525217845bcb5db2348be65c7
LIYVkk <743044810@qq.com>
2021-04-10 22:07:37 +0800

    用了join

--- LIYVkk/1002.py
+++ LIYVkk/1002.py
@@ -1,13 +1,12 @@
 n=input()
-l=len(n)
 t=0
-for i in range(l):
-    t+=int(n[i])
-t=str(t)
+for i in n:
+    t+=int(i)
+str_word=str(t)
 dic={'0':"ling",'1':"yi",'2':"er",'3':"san",'4':"si",'5':"wu",'6':"liu",'7':"qi",'8':"ba",'9':"jiu"}
-for i in t:
+list_word=[]
+for i in str_word:
     if i in dic:
-        if i != t[len(t)-1]:
-            print(dic[i],end=' ')
-        else:
-            print(dic[i])
+        list_word.append(dic[i])
+str_lian=" "
+print(str_lian.join(list_word))
